f6f3719c4d2c489f05664457fcb42b18c6d77184
MADNA <madnamv@gmail.com>
2024-10-17 20:05:10 -0400

    Add: port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000
+console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
